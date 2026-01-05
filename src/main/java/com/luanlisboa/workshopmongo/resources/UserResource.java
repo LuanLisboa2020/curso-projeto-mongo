09fb50c0649df5043602e1c69b242c0eec924bd0
Luan Lisboa <lluan4593@gmail.com>
2026-01-05 19:28:05 -0300

    Deleção de usuarios com DELETE

--- src/main/java/com/luanlisboa/workshopmongo/resources/UserResource.java
+++ src/main/java/com/luanlisboa/workshopmongo/resources/UserResource.java
@@ -2,10 +2,12 @@ package com.luanlisboa.workshopmongo.resources;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,5 +46,11 @@ public class UserResource {
 		    obj = service.insert(obj);
 		    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		    return ResponseEntity.created(uri).build();
+	    }
+	 
+	 @DeleteMapping("/{id}")
+	    public ResponseEntity<Map<String, String>> delete(@PathVariable String id) {
+		    service.delete(id);
+	        return ResponseEntity.ok(Map.of("message", "Usuário removido com sucesso"));
 	    }
 }
